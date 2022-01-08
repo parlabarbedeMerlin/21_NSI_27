04776ba780e412429487082549706ab168585833
BuildTools <unconfigured@null.spigotmc.org>
2022-01-08 20:55:12 +0100

    Refactor file + syntaxe

--- 21_NSI_27.py
+++ 21_NSI_27.py
@@ -1,100 +1,110 @@
-#Exercice 1 => 00:00
+# Exercice 1 => 00:00
 
-def moyenne(L:list)->float: #on définie la fonction et lui indique le paramètre L ainsi que son type. On indique aussi le type retourné
+# on définie la fonction et lui indique le paramètre L ainsi que son type. On indique aussi le type retourné
+def moyenne(liste: list) -> float:
     """
     renvoie la moyenne d'une liste d'entier
-    :param L: liste d'entiers
+    :param liste: liste d'entiers
     :return: float
     """
-    total = 0 #on intialise le total
-    for i in L: #on parcours tous les éléments de la liste en passant à chaque fois au suivant
-        assert not(i is int), "La liste contient un élément qui n'est pas en entier" # vérifie si la liste contient un élément diférent d'un entier
-        total = total+i #on ajoute au total l'élément parcourut au moment précis ou l'instruction est exécuté
-    return total/len(L) #on renvoie la moyenne
+    total = 0  # on intialise le total
+    for i in liste:  # on parcours tous les éléments de la liste en passant à chaque fois au suivant
 
-print(moyenne([10,20,30,40,60,110])) #test de la fonction moyenne()
+        # vérifie si la liste contient un élément diférent d'un entier
+        assert not (i is int), "La liste contient un élément qui n'est pas en entier"
 
-#Exercice 1 fini en 1:45
+        # on ajoute au total l'élément parcourut au moment précis ou l'instruction est exécuté
+        total = total + i
 
-#Exercice 2 => 02:30
+    return total / len(liste)  # on renvoie la moyenne
 
-#définition du motif
-coeur = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], \
-         [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0], \
-         [0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0], \
-         [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0], \
-         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], \
-         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0], \
-         [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], \
-         [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0], \
-         [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0], \
-         [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0], \
-         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0], \
+
+print(moyenne([10, 20, 30, 40, 60, 110]))  # test de la fonction moyenne()
+
+# Exercice 1 fini en 1:45
+
+# Exercice 2 => 02:30
+
+# définition du motif
+coeur = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0],
+         [0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0],
+         [0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0],
+         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+         [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+         [0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+         [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 
 def affiche(dessin):
-    '''affichage d'une grille : les 1 sont représentés par
+    """affichage d'une grille : les 1 sont représentés par
         des "*" , les 0 par deux espaces "  "
     :param dessin: liste de liste de caractères
     :return: None (affiche dans la console)
-    '''
+    """
     for ligne in dessin:
         for col in ligne:
             if col == 1:
-                print(" *",end="")
+                print(" *", end="")
             else:
-                print("  ",end="")
+                print("  ", end="")
         print()
 
 
-def zoomListe(liste_depart,k):
-    '''renvoie une liste contenant k fois chaque
+def zoomliste(liste_depart, k):
+    """renvoie une liste contenant k fois chaque
        élément de liste_depart
     :param liste_depart: liste de caractères
     :param k: entier
     :return: renvoie la liste zoomé par k
-    '''
-    liste_zoom = [] #la list est vide au départ
-    for elt in  liste_depart: #on parcours chaque motif (vide ou remplie de la liste)
+    """
+    liste_zoom = []  # la list est vide au départ
+    for elt in liste_depart:  # on parcours chaque motif (vide ou remplie de la liste)
         for i in range(k):
-            liste_zoom.append(elt) #on l'ajoute fois k
-    return liste_zoom #on renvoie la ligne zoomé par k
+            liste_zoom.append(elt)  # on l'ajoute fois k
+    return liste_zoom  # on renvoie la ligne zoomé par k
+
 
 # au bout de 07:30
 
-def zoomDessin(grille,k):
-    '''renvoie une grille où les lignes sont zoomées k fois
+def zoomdessin(grille, k):
+    """renvoie une grille où les lignes sont zoomées k fois
        ET répétées k fois
     :param grille: liste de list de caractère
     :param k: entier
     :return: grille zoomé par k
-    '''
-    grille_zoom=[] #grille zoomé définie comme liste vide
-    for elt in grille: #on parcours chaque ligne de la list
-        liste_zoom = zoomListe(elt,k) #on stock dans une variable la ligne zoomé par k
+    """
+    grille_zoom = []  # grille zoomé définie comme liste vide
+    for elt in grille:  # on parcours chaque ligne de la list
+        liste_zoom = zoomliste(elt, k)  # on stock dans une variable la ligne zoomé par k
         for i in range(k):
-            grille_zoom.append(liste_zoom) #on ajoute fois k la liste zoomé
-    return grille_zoom #on renvoie la grille (tableau) zoomé
+            grille_zoom.append(liste_zoom)  # on ajoute fois k la liste zoomé
+    return grille_zoom  # on renvoie la grille (tableau) zoomé
 
-#Au bout de 10:12
+
+# Au bout de 10:12
 
 affiche(coeur)
 
-affiche(zoomDessin(coeur,3))
+affiche(zoomdessin(coeur, 3))
 
-#le code à été fini au bout de 12 minutes et 26 secondes
+
+# le code à été fini au bout de 12 minutes et 26 secondes
 
 # le code a été fini d'être commenté au bout de 29 minutes et 46 secondes
 
 
 # Bonus (NECESSITE TKINTER)
 
-def afficher_couleur(dessin,color = "WHITE"):
+def afficher_couleur(dessin, color="WHITE"):
     """
     affiche le dessin dans la couleur spécifié
     :param dessin:
-    :param couleur:
+    :param color:
     :return: None (affiche un dessin dans la console)
     """
     if color == "RED":
@@ -112,7 +122,7 @@ def afficher_couleur(dessin,color = "WHITE"):
     elif color == "WHITE":
         print("\u001b[1;37m")
     else:
-        print("\u001b[1;37m")        
+        print("\u001b[1;37m")
 
     affiche(dessin)
 
@@ -154,6 +164,7 @@ def showonwin(dessin):
     # show window
     root.mainloop()
 
-showonwin(zoomDessin(coeur,3))
+
+showonwin(zoomdessin(coeur, 3))
 
 # le bonus a été fini au bout de 1h 2 minutes et 23 secondes
