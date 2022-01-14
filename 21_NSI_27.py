982b1d82ada45fb5b405611237fab689d82d5994
parlabarbedeMerlin <tristan.tr.renard@gmail.com>
2022-01-14 09:58:09 +0100

    final with comments

--- 21_NSI_27.py
+++ 21_NSI_27.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Canvas, Frame, BOTH
+from tkinter import Tk, Canvas, Frame, BOTH  # import de tkinter pour le bonus
 
 
 # Exercice 1 => 00:00
@@ -21,9 +21,8 @@ def moyenne(liste: list) -> float:
 
     return total / len(liste)  # on renvoie la moyenne
 
-print(moyenne([10, 20, 30, 40, 60, 110]))  # on affiche la moyenne de la liste
 
-# test de la fonction moyenne()
+print(moyenne([10, 20, 30, 40, 60, 110]))  # test de la fonction moyenne()
 
 # Exercice 1 fini en 1:45
 
@@ -50,12 +49,12 @@ def affiche(dessin):
     :param dessin: liste de liste de caractères
     :return: None (affiche dans la console)
     """
-    for ligne in dessin:
-        for col in ligne:
-            if col == 1:
+    for ligne in dessin: # on parcours chaques lignes du dessin
+        for col in ligne: #on parcours chaque élément de la ligne
+            if col == 1: #on regarde si quel est le caractère a afficher
                 print(" *", end="")  # on affiche un "*" avec un espace après pour ne pas avoir le dessin étiré
             else:
-                print("  ", end="")
+                print("  ", end="") # sinon on affiche deux espaces pour ne pas déformer le dessin
         print()
 
 
@@ -68,8 +67,8 @@ def zoomliste(liste_depart, k):
     """
     liste_zoom = []  # la list est vide au départ
     for elt in liste_depart:  # on parcours chaque motif (vide ou remplie de la liste)
-        for i in range(k):
-            liste_zoom.append(elt)  # on l'ajoute fois k
+        for i in range(k): #on executes l'instruction imbriqué k fois
+            liste_zoom.append(elt)  # on on ajoute element
     return liste_zoom  # on renvoie la ligne zoomé par k
 
 
@@ -85,16 +84,16 @@ def zoomdessin(grille, k):
     grille_zoom = []  # grille zoomé définie comme liste vide
     for elt in grille:  # on parcours chaque ligne de la list
         liste_zoom = zoomliste(elt, k)  # on stock dans une variable la ligne zoomé par k
-        for i in range(k):
-            grille_zoom.append(liste_zoom)  # on ajoute fois k la liste zoomé
+        for i in range(k): # on répète k fois les instructions
+            grille_zoom.append(liste_zoom)  # on ajoute la liste zoomé
     return grille_zoom  # on renvoie la grille (tableau) zoomé
 
 
 # Au bout de 10:12
 
-affiche(coeur)
+affiche(coeur) # test de la fonction affiche()
 
-affiche(zoomdessin(coeur, 3))
+affiche(zoomdessin(coeur, 3)) # test de la fonction zoomdessin()
 
 
 # le code à été fini au bout de 12 minutes et 26 secondes
@@ -111,36 +110,36 @@ def afficher_couleur(dessin, color="WHITE"):
     :param color:
     :return: None (affiche un dessin dans la console)
     """
-    if color == "RED":
-        print("\u001b[1;31m")
-    elif color == "GREEN":
-        print("\u001b[1;32m")
-    elif color == "YELLOW":
-        print("\u001b[1;33m")
-    elif color == "BLUE":
-        print("\u001b[1;34m")
-    elif color == "MAGENTA":
-        print("\u001b[1;35m")
-    elif color == "CYAN":
-        print("\u001b[1;36m")
-    elif color == "WHITE":
-        print("\u001b[1;37m")
-    else:
-        print("\u001b[1;37m")
+    if color == "RED": # si la couleur est rouge
+        print("\u001b[1;31m") # on met la couleur rouge
+    elif color == "GREEN": # si la couleur est verte
+        print("\u001b[1;32m") # on met la couleur verte
+    elif color == "YELLOW": # si la couleur est jaune
+        print("\u001b[1;33m") # on met la couleur jaune
+    elif color == "BLUE": # si la couleur est bleue
+        print("\u001b[1;34m") # on met la couleur bleue
+    elif color == "MAGENTA": # si la couleur est magenta
+        print("\u001b[1;35m") # on met la couleur magenta
+    elif color == "CYAN": # si la couleur est cyan
+        print("\u001b[1;36m") # on met la couleur cyan
+    elif color == "WHITE": # si la couleur est blanche
+        print("\u001b[1;37m") # on met la couleur blanche
+    else: # sinon on met la couleur par défaut
+        print("\u001b[1;37m") # on met la couleur blanche
 
     affiche(dessin)
 
-    # on remet la couleur par défaut
-    print("\u001b[0m")
+    #après avoir fini d'afficher le dessin, on remet la couleur par défaut
+    print("\u001b[0m")# on remet la couleur par défaut
 
 
-# create a window
-root = Tk()
-root.title("Dessin")
-root.geometry("800x800")
+# Bonus (NECESSITE TKINTER)
+root = Tk() # on crée une fenêtre
+root.title("Dessin") # on donne un titre à la fenêtre
+root.geometry("800x800") # on donne une taille à la fenêtre
 
 
-def showonwin(dessin, win):
+def showonwin(dessin, win): # on crée une fonction qui affiche un dessin dans une fenêtre
     """
     print the motif of dessin on grid on window using tkinter
     :param win: window
@@ -149,37 +148,24 @@ def showonwin(dessin, win):
     """
 
     # create a canvas
-    canvas = Canvas(win, width=800, height=800)
-    canvas.pack(fill=BOTH, expand=1)
+    canvas = Canvas(win, width=800, height=800) # on crée un canvas de 800x800 pixels
+    canvas.pack(fill=BOTH, expand=1) # on met le canvas dans la fenêtre
 
     # create a frame
-    frame = Frame(win)
-    frame.pack()
+    frame = Frame(win) # on crée un cadre
+    frame.pack() # on met le cadre dans la fenêtre
 
     # create a grid
-    for i in range(len(dessin)):
-        for j in range(len(dessin[i])):
-            if dessin[i][j] == 1:
-                # create a rectangle
-                canvas.create_rectangle(j * 20, i * 20, (j + 1) * 20, (i + 1) * 20, fill="black")
-            else:
-                # create a rectangle
-                canvas.create_rectangle(j * 20, i * 20, (j + 1) * 20, (i + 1) * 20, fill="white", outline="black")
-
-    # show window
-    win.mainloop()
+    for i in range(len(dessin)): # on parcours chaque indices des lignes du dessin
+        for j in range(len(dessin[i])): # puis chaques caractères
+            if dessin[i][j] == 1: # si le caractère est un 1
+                canvas.create_rectangle(j * 20, i * 20, (j + 1) * 20, (i + 1) * 20, fill="black") # on crée un rectangle noir
+            else: # sinon
+                canvas.create_rectangle(j * 20, i * 20, (j + 1) * 20, (i + 1) * 20, fill="white", outline="black") # on crée un rectangle blanc
+    win.mainloop() # on affiche la fenêtre
 
 
-showonwin(coeur, root)
+showonwin(coeur, root) # on affiche le dessin coeur dans la fenêtre
 
 
 # le bonus a été fini au bout de 1h 2 minutes et 23 secondes
-
-# closewin func
-def closewin(win):
-    """
-    close the window
-    :return: None
-    """
-    win.destroy()
-    win.quit()
